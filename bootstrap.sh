7f938bd6bdc2570ef5628d41c83d135e914c2059
Aaron Lake <aaron.lake@gmail.com>
2012-12-03 17:47:47 -0500

    Initial commit.

---
+++ bootstrap.sh
@@ -0,0 +1,34 @@
+#################################
+# copy from https://github.com/carlhuda/janus/blob/master/bootstrap.sh
+
+function die()
+{
+  echo "${@}"
+  exit 1
+}
+
+DOTFILES=$HOME/.dotfiles/
+
+# Add <strong>.old</strong> to any existing Vim file in the home directory
+for i in $DOTFILES/{.gitconfig .brew .exports .functions .gitconfig .gitignore .inputrc .osx}; do
+  if [[ ( -e $i ) || ( -h $i ) ]]; then
+    echo "${i} has been renamed to ${i}.old"
+    mv "${i}" "${i}.old" || die "Could not move ${i} to ${i}.old"
+  fi
+done
+#################################
+
+git clone --recursive git://github.com/aaronlake/dotfiles.git $HOME/.dotfiles \
+    || die "Could not clone the repository to ${HOME}/.dotfiles"
+
+echo "Link configuration files"
+ln -s $DOTFILES/.gitconfig $HOME/.gitconfig
+ln -s $DOTFILES/.brew $HOME/.brew
+ln -s $DOTFILES/.exports $HOME/.exports
+ln -s $DOTFILES/.functions $HOME/.functions
+ln -s $DOTFILES/.gitignore $HOME/.gitignore
+ln -s $DOTFILES/.inputrc $HOME/.inputrc
+ln -s $DOTFILES/.osx $HOME/.osx
+
+echo "Install successfully."
+
