06613506b4e8f25223a274c56b630f488cf3a67a
Aaron Lake <aaron.lake@gmail.com>
2018-11-27 11:00:32 -0600

    Update global gitignore

--- bootstrap.sh
+++ bootstrap.sh
@@ -36,11 +36,14 @@ echo "Link configuration files"
 ln -s $DOTFILES/gitconfig $HOME/.gitconfig
 ln -s $DOTFILES/exports $HOME/.exports
 ln -s $DOTFILES/functions $HOME/.functions
-ln -s $DOTFILES/gitignore $HOME/.gitignore
+ln -s $DOTFILES/gitignore $HOME/.gitignore_global
 ln -s $DOTFILES/inputrc $HOME/.inputrc
 ln -s $DOTFILES/gemrc $HOME/.gemrc
 ln -s $DOTFILES/zshrc $HOME/.zshrc
 ln -s $DOTFIlES/dircolors-solarized/dircolors.256dark $HOME/.dircolors
+
+echo "Setting global gitignore"
+git config --global core.excludesfile ~/.gitignore_global
 
 echo "Install successfully."
 
